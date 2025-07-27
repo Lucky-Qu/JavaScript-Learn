f1ef926f8f4a5e187aa7f104825d7a4151b9ba60
LuckyQu <super.lucky.qu@gmail.com>
2025-07-27 16:48:07 +0800

    学习了简单的输入输出语句以便学习

---
+++ 01_Learn/02_SimpleIO/SimpleIO.js
@@ -0,0 +1,10 @@
+// 输出：
+// alert 用于在浏览器窗口提示
+alert("Hello There")
+// console用于在控制台输出
+console.log("Hi!")
+// document.write 可以将内容书写到HTML中
+document.write("<h1>Hello form JS</h1>")
+//输入
+//prompt可以弹出窗口输入
+prompt("Type something, Anything")
